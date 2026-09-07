56d7e09dff675ead5ff7c01b5200a35648e86605
bwithtea <bwithtea@gmail.com>
2026-09-07 10:30:05 -0400

    initial spring boot skeleton

---
+++ src/main/java/com/tea/time_mileage_tracker/TimeMileageTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tea.time_mileage_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeMileageTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeMileageTrackerApplication.class, args);
+	}
+
+}
